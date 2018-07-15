a9e9a41f374dc277efd04d4aad07e39dee0f24d6
Liam Curry <liam@sagan.software>
2018-07-15 14:33:46 -0600

    test

--- scripts/post-receive.sh
+++ scripts/post-receive.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 WORK_DIR=/repo/eosbp.report
 GIT_DIR=/repo/eosbp.report.git
+BUILD_TREE=build
+BUILD_WORK_DIR=$WORK_DIR/$BUILD_TREE
+BUILD_GIT_DIR=$GIT_DIR/worktrees/$BUILD_TREE
 
 cd $GIT_DIR
-rm -Rf $WORK_DIR/build
-git worktree add $WORK_DIR/build gh-pages
+rm -Rf $BUILD_WORK_DIR
+git worktree remove $BUILD_TREE
+git worktree add $BUILD_WORK_DIR gh-pages
 
 cd $WORK_DIR
 yarn install
 yarn build-prod
 node ./scripts/fetch_json.js
+
+# git --git-dir=$BUILD_GIT_DIR --work-tree=$BUILD_WORK_DIR add .
+# git --git-dir=$BUILD_GIT_DIR --work-tree=$BUILD_WORK_DIR commit -am "auto-update"
+# git --git-dir=$BUILD_GIT_DIR --work-tree=$BUILD_WORK_DIR push origin gh-pages
