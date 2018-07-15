a85c6c18e3b2abd13d4f1a27e8a1d76468e87af5
Liam Curry <liam@sagan.software>
2018-07-15 11:30:36 -0600

    removed .env files, added post-receive script

---
+++ scripts/post-receive.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+WORK_DIR=/repo/eosbp.report
+GIT_DIR=/repo/eosbp.report.git
+
+cd GIT_DIR
+rm -Rf $WORK_DIR/build
+git worktree add $WORK_DIR/build gh-pages
+
+cd $WORK_DIR
+yarn install
+yarn build-prod
+node ./scripts/fetch_json.js
