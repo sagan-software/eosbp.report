a613fb1d7bba53b72a8cb5c29a4d9539452b05e4
Liam Curry <liam@sagan.software>
2018-07-19 10:45:23 -0600

    clear yarn cache

--- scripts/post-receive.sh
+++ scripts/post-receive.sh
@@ -11,6 +11,7 @@ git worktree remove $BUILD_TREE
 git worktree add $BUILD_WORK_DIR gh-pages
 
 cd $WORK_DIR
+yarn cache clean
 yarn install
 yarn upgrade @sagan-software/bs-bignumber
 yarn upgrade @sagan-software/bs-eos
